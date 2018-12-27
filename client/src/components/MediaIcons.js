7591ef61800fa7e1a503ac6f7b57b1496c85582b
Fish-bowl <symbolt.ap@gmail.com>
2018-12-26 18:07:17 -0700

    changed to buttons as icons

--- client/src/components/MediaIcons.js
+++ client/src/components/MediaIcons.js
@@ -2,15 +2,24 @@ import React from 'react'
 import {
   Icon,
   Segment,
+  Button,
 } from 'semantic-ui-react'
 
 class MediaIcons extends React.Component {
+
+  handleClick = () => {
+    alert("i've been clicked")
+  }
+
   render() {
     return(
       <Segment compact style={styles.container} inverted>
-        <Icon size='large' name='facebook' color='blue' />
+        <Button icon='facebook' size='large' basic color='blue' circular onClick={this.handleClick} />
+        <Button icon='instagram' size='large' basic color='grey' circular onClick={this.handleClick} />
+        <Button icon='mail' size='large' basic color='red' circular onClick={this.handleClick} />
+        {/* <Icon size='large' name='facebook' color='blue' />
         <Icon size='large' name='instagram' color='grey' />
-        <Icon size='large' name='mail' golor='red' />
+        <Icon size='large' name='mail' golor='red' /> */}
       </Segment>
     )
   }
